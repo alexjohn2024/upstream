5dd3484ac2e8368c57992e575acf6a46112836c7
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-23 15:25:44 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -212,9 +212,9 @@ def get_readable_message():
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
     msg += f"\n<b>UPTM</b>: {currentTime}"
-    msg += f"<b>DL</b>: {get_readable_file_size(dl_speed)}/s"
+    msg += f"<b>  DL</b>: {get_readable_file_size(dl_speed)}/s"
     msg += f"\n<b>Free</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
-    msg += f"<b>UL</b>: {get_readable_file_size(up_speed)}/s"
+    msg += f"<b>  UL</b>: {get_readable_file_size(up_speed)}/s"
     return msg, button
 
 
