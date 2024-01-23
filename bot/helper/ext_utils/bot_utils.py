4d746ed60c1f8b69f7876929a37ec6c9ac45c620
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-23 17:44:51 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>⚡Powered By ≫ <a href="t.me/lovelyhacker_143">༺ Nagesh Ravilla ༻</a></b>\n\n'
+    msg = '<b>⚡Powered By : <a href="t.me/lovelyhacker_143">Nagesh Ravilla</a></b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
