45fef01f63d4bc40b5d34997f1f6c18977244cbd
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-23 18:26:54 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>⚡Powered By : <a href="t.me/lovelyhacker_143">Nagesh Ravilla</a></b>\n\n'
+    msg = '<b>⚡Powered By : <a href="t.me/lovelyhacker_143">lovelyhacker143</a></b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
@@ -211,10 +211,11 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
-    msg += f"\n<b>UPTM</b>: {currentTime}"
-    msg += f"<b>  DL</b>: {get_readable_file_size(dl_speed)}/s"
-    msg += f"\n<b>Free</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
-    msg += f"<b>  UL</b>: {get_readable_file_size(up_speed)}/s"
+    msg += f"<b>• Tasks</b>: {tasks}{bmax_task}"
+    msg += f"\n<b>• Bot uptime</b>: {currentTime}"
+    msg += f"\n<b>• Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
+    msg += f"\n<b>• Uploading speed</b>: {get_readable_file_size(up_speed)}/s"
+    msg += f"\n<b>• Downloading speed</b>: {get_readable_file_size(dl_speed)}/s"
     return msg, button
 
 
