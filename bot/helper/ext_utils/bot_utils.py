5cae3c00be26adbc9bdf825b2cab4b526ec81e86
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-24 18:41:40 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -148,7 +148,7 @@ def progress_bar(pct):
     p = min(max(pct, 0), 100)
     cFull = int((p + 5)// 10)
     p_str = '🌺' * cFull
-    p_str += '❀' * (10 - cFull)
+    p_str += '🌼' * (10 - cFull)
     return p_str
 
 
@@ -172,9 +172,9 @@ def get_readable_message():
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
         msg += f"<b>🌍{escape(f'{download.name()}')}</b>\n"
         msg += f"by {source(download)}\n\n"
-        msg += f"<b>{download.status()}...</b>"
+        msg += f"<b>{download.status()}...</b>\n"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n<code>{progress_bar(download.progress())}</code> {download.progress()}\n"
+            msg += f"\n⊱<code>{progress_bar(download.progress())}</code>⊰ {download.progress()}\n"
             msg += f"\n{download.processed_bytes()} of {download.size()}\n"
             msg += f"\n🔰Speed: {download.speed()}"
             msg += f'\n🔰Estimated: {download.eta()}'
@@ -211,11 +211,11 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
-    msg += f"<b>• Tasks</b>: {tasks}{bmax_task}"
-    msg += f"\n<b>• Bot uptime</b>: {currentTime}"
-    msg += f"\n<b>• Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
-    msg += f"\n<b>• Uploading speed</b>: {get_readable_file_size(up_speed)}/s"
-    msg += f"\n<b>• Downloading speed</b>: {get_readable_file_size(dl_speed)}/s"
+    msg += f"<b>❅ Tasks</b>: {tasks}{bmax_task}"
+    msg += f"\n<b>❅ Bot uptime</b>: {currentTime}"
+    msg += f"\n<b>❅ Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
+    msg += f"\n<b>❅ Uploading speed</b>: {get_readable_file_size(up_speed)}/s"
+    msg += f"\n<b>❅ Downloading speed</b>: {get_readable_file_size(dl_speed)}/s"
     return msg, button
 
 
