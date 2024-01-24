9ac508c7d48d21abdc43c3a347723ec1e4c206ca
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-24 14:07:26 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -176,22 +176,22 @@ def get_readable_message():
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<code>{progress_bar(download.progress())}</code> {download.progress()}"
             msg += f"\n{download.processed_bytes()} of {download.size()}"
-            msg += f"\nSpeed: {download.speed()}"
-            msg += f'\nETA: {download.eta()}'
+            msg += f"\n🔰Speed: {download.speed()}"
+            msg += f'\n🔰ETA: {download.eta()}'
             if hasattr(download, 'seeders_num'):
                 try:
                     msg += f"\nSeeders: {download.seeders_num()} | Leechers: {download.leechers_num()}"
                 except:
                     pass
         elif download.status() == MirrorStatus.STATUS_SEEDING:
-            msg += f"\nSize: {download.size()}"
-            msg += f"\nSpeed: {download.upload_speed()}"
-            msg += f"\nUploaded: {download.uploaded_bytes()}"
-            msg += f"\nRatio: {download.ratio()}"
-            msg += f"\nTime: {download.seeding_time()}"
+            msg += f"\n🔰Size: {download.size()}"
+            msg += f"\n🔰Speed: {download.upload_speed()}"
+            msg += f"\n🔰Uploaded: {download.uploaded_bytes()}"
+            msg += f"\n🔰Ratio: {download.ratio()}"
+            msg += f"\n🔰Time: {download.seeding_time()}"
         else:
-            msg += f"\nSize: {download.size()}"
-        msg += f"\nPast: {get_readable_time(time() - download.message.date.timestamp())}"
+            msg += f"\n🔰Size: {download.size()}"
+        msg += f"\n🔰Past: {get_readable_time(time() - download.message.date.timestamp())}"
         msg += f"\n✋🏻/stop_{download.gid()[:8]}\n\n"
     if len(msg) == 0:
         return None, None
