865ebf21bbd842ea0b0fa826136e4afe235a3e6e
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-26 19:29:05 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -145,8 +145,8 @@ async def fetch_user_tds(user_id, force=False):
 def progress_bar(pct):
     pct = float(str(pct).strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
+    cFull = int(p // 7)
+    cPart = int(p % 7 - 1)
     p_str = '■' * cFull
     if cPart >= 0:
         p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
