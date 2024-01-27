5d6be86bd60fe2ea92028758ea9c5143a57916e9
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-01-27 11:56:02 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -210,6 +210,13 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
+        /n
+        ╭──────────.★..─╮
+┊ ⋆ ┊ . ┊ ┊
+┊ ┊⋆ ┊ .
+┊ ┊ ⋆˚ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭
+✧. ┊ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭ ⁭
+⋆ ★
     msg += f"<b>❆ Tasks</b>: {tasks}{bmax_task}"
     msg += f"\n<b>❆ Bot uptime</b>: {currentTime}"
     msg += f"\n<b>❆ Free disk space</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
