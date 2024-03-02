08d247bf5900865c78475c174b3b18365c08ee64
alexjohn2024 <155365771+alexjohn2024@users.noreply.github.com>
2024-03-02 12:40:17 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -21,7 +21,7 @@ async def mirror_status(_, message):
         quote = Quote.print().split('―', 1)[0].strip().replace("“", "").replace("”", "")
 
         msg = f'<b>{quote}</b>\n\n'
-        msg += f'<blockquote>'No downloads are currently in progress.</blockquote>\n'
+        msg += 'No downloads are currently in progress.\n'
         msg += f"\n<b>❅ Bot uptime</b>: {currentTime}"
         msg += f"\n<b>❅ Free disk space</b>: {free}"
 
